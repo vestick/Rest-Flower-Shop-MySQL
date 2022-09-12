f03781bbddb42fb1ebbbe8588473ffc75a35c993
vestick <xorayesno@gmail.com>
2022-09-12 16:24:06 +0300

    Initial commit

---
+++ src/main/java/com/example/app/RestFlowerShopMySQL/RestFlowerShopMySqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.app.RestFlowerShopMySQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestFlowerShopMySqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestFlowerShopMySqlApplication.class, args);
+	}
+
+}
